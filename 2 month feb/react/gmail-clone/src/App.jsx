73a0f13f5c26956ef41b3610aeb17058db94f78d
sakshishejwal28 <shejwalsakshi381@gmail.com>
2026-02-10 12:52:56 +0530

    done with changes

---
+++ 2 month feb/react/gmail-clone/src/App.jsx
@@ -0,0 +1,71 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-white' >hoc tech</h1>
+      <p>I'm a dedicated MERN Stack developer with 2 years of experience in building scalable and user-friendly web applications. Proficient in MongoDB, Express.js, React, and Node.js, I specialize in creating seamless full-stack solutions.
+
+        My passion lies in solving complex problems and delivering clean, efficient code. I thrive in collaborative environments and am always eager to learn new technologies to enhance my skill set.
+        I'm a dedicated MERN Stack developer with 2 years of experience in building scalable and user-friendly web applications. Proficient in MongoDB, Express.js, React, and Node.js, I specialize in creating seamless full-stack solutions.
+
+        My passion lies in solving complex problems and delivering clean, efficient code. I thrive in collaborative environments and am always eager to learn new technologies to enhance my skill set.
+
+      </p>
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>skill</li>
+        <li>project</li>
+      </ul>
+      <div className="table"> 
+            <div className="row">
+        <div className="col-6">
+          
+          <table>
+            <tr>
+              <th>name </th>
+              <th>age </th>
+              <th>clg </th>
+              <th>name </th>
+              <th>age </th>
+              <th>clg </th>
+
+            </tr>
+
+            <tr>
+              <td>hoc</td>
+              <td>27</td>
+              <td>sgm</td>
+              <td>hoc</td>
+              <td>27</td>
+              <td>sgm</td>
+            </tr>
+            <tr>
+              <td>hoc</td>
+              <td>27</td>
+              <td>sgm</td>
+              <td>hoc</td>
+              <td>27</td>
+              <td>sgm</td>
+            </tr>
+            <tr>
+              <td>hoc</td>
+              <td>27</td>
+              <td>sgm</td>
+              <td>hoc</td>
+              <td>27</td>
+              <td>sgm</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
